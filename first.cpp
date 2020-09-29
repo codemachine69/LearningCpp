443c06298c0644db32a2cb1c8d49e14611f5df8e
codemachine69 <66106096+codemachine69@users.noreply.github.com>
2020-09-29 23:57:57 +0530

    Update first.cpp
    Printing the sum of the vector elements

--- first.cpp
+++ first.cpp
@@ -3,14 +3,17 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+
     vector<int> v;
     for (int i = 0; i < 10; i++)
     {
         v.push_back(i);
     }
+    int sum = 0;
     for (int i = 0; i < v.size(); i++)
     {
         cout << v[i] << endl;
+        sum += v[i];
     }
+    cout << sum << endl;
 }
